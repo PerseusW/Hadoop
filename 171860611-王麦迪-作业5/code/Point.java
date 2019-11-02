b242f7c4efa97f3a1133b36ff84e83de4750d486
Percy <1772761454@qq.com>
2019-11-02 10:15:40 +0800

    Made point comparable

--- 171860611-王麦迪-作业5/code/Point.java
+++ 171860611-王麦迪-作业5/code/Point.java
@@ -1,12 +1,12 @@
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 
-public class Point implements Writable
+public class Point implements WritableComparable<Point>
 {
-    int x;
-    int y;
+    private int x;
+    private int y;
 
     public Point() {
         x = 0;
@@ -17,6 +17,14 @@ public class Point implements Writable
         String[] tuple = line.split(",");
         x = Integer.parseInt(tuple[0]);
         y = Integer.parseInt(tuple[1]);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public String toString() {
@@ -31,5 +39,23 @@ public class Point implements Writable
     public void write(DataOutput out) throws IOException {
         out.writeInt(x);
         out.writeInt(y);
+    }
+
+    public int compareTo(Point other) {
+        if (x > other.getX()) {
+            return 1;
+        }
+        else if (x < other.getX()) {
+            return -1;
+        }
+        else if (y > other.getY()) {
+            return 1;
+        }
+        else if (y < other.getY()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
     }
 }
