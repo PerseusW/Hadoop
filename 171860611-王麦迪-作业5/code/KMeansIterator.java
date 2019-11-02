68617da4d11a579c9f2a4817e50a8823328a2ad0
Percy <1772761454@qq.com>
2019-11-02 10:17:10 +0800

    Renamed file

---
+++ 171860611-王麦迪-作业5/code/KMeansIterator.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class KMeansIterator
+{
+
+    public static class KMeansMapper extends Mapper<LongWritable, Text, IntWritable, Point>
+    {
+        
+    }
+}
