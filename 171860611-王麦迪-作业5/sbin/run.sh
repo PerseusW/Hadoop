9f977550437e56cd303659b5f887dff49c4210c0
Percy <1772761454@qq.com>
2019-11-03 14:06:41 +0800

    Homework finished

--- 171860611-王麦迪-作业5/sbin/run.sh
+++ 171860611-王麦迪-作业5/sbin/run.sh
@@ -1,6 +1,6 @@
 rm -r ~/Desktop/output
 ${HADOOP}/bin/hdfs dfs -rm -r /user/percy/output
-${HADOOP}/bin/hadoop jar ~/Desktop/jar/KMeans.jar Main /user/percy/input /user/percy/output 4 3
+${HADOOP}/bin/hadoop jar ~/Desktop/jar/KMeans.jar Main /user/percy/input /user/percy/output 4 4
 ${HADOOP}/bin/hdfs dfs -get /user/percy/output ~/Desktop
 echo
 echo Initial Clusters:
